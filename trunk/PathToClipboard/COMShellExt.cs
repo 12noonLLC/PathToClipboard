67a9f813e894a5b8aa9136c12789a2b667546910
Stefan Tucker <stefan@skst.com>
2008-01-12 02:29:41 +0000

    Moved remotely to eliminate top-level Development folder
    git-svn-id: svn://localhost/trunk@2130 11f071c2-d427-764a-a8f1-537ef3230c9f

---
+++ trunk/PathToClipboard/COMShellExt.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+//using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+namespace MyCOMDefinitions
+{
+	public enum CMF : uint
+	{
+		CMF_NORMAL		= 0x00000000,
+		CMF_DEFAULTONLY	= 0x00000001,
+		CMF_VERBSONLY	= 0x00000002,
+		CMF_EXPLORE		= 0x00000004,
+		CMF_NOVERBS		= 0x00000008,
+		CMF_CANRENAME	= 0x00000010,
+		CMF_NODEFAULT	= 0x00000020,
+		CMF_INCLUDESTATIC= 0x00000040,
+		CMF_RESERVED	= 0xffff0000      // View specific
+	}
+
+	// GetCommandString uFlags
+	public enum GCS: uint
+	{
+		VERBA =			0x00000000,     // canonical verb
+		HELPTEXTA =		0x00000001,     // help text (for status bar)
+		VALIDATEA =		0x00000002,     // validate command exists
+		VERBW =			0x00000004,     // canonical verb (unicode)
+		HELPTEXTW =		0x00000005,     // help text (unicode version)
+		VALIDATEW =		0x00000006,     // validate command exists (unicode)
+		UNICODE =		0x00000004,     // for bit testing - Unicode string
+		VERB =			GCS.VERBA,
+		HELPTEXT =		GCS.HELPTEXTA,
+		VALIDATE =		GCS.VALIDATEA
+	}
+
+	[StructLayout(LayoutKind.Sequential, CharSet=CharSet.Unicode)]
+	public struct INVOKECOMMANDINFO
+	{
+		//NOTE: When SEE_MASK_HMONITOR is set, hIcon is treated as hMonitor
+		public UInt32 cbSize;						// sizeof(CMINVOKECOMMANDINFO)
+		public UInt32 fMask;						// any combination of CMIC_MASK_*
+		public IntPtr hwnd;						// might be NULL (indicating no owner window)
+		public UInt32 verb;
+		[MarshalAs(UnmanagedType.LPStr)]
+		public string parameters;				// might be NULL (indicating no parameter)
+		[MarshalAs(UnmanagedType.LPStr)]
+		public string directory;				// might be NULL (indicating no specific directory)
+		public Int32 nShow;						// one of SW_ values for ShowWindow() API
+		public UInt32 dwHotKey;
+		public IntPtr hIcon;
+	}
+
+	[ComImport(), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), GuidAttribute("000214e8-0000-0000-c000-000000000046")]
+	public interface IShellExtInit
+	{
+		[PreserveSig()]
+		UInt32 Initialize(IntPtr pidlFolder, IntPtr lpdobj, IntPtr hKeyProgID);
+	}
+
+
+	[ComImport(), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), GuidAttribute("000214e4-0000-0000-c000-000000000046")]
+	public interface IContextMenu
+	{
+		[PreserveSig()]
+		UInt32 QueryContextMenu(IntPtr hmenu, UInt32 iMenu, UInt32 idCmdFirst, UInt32 idCmdLast, UInt32 uFlags);
+
+		[PreserveSig()]
+		UInt32 InvokeCommand(IntPtr pici);
+
+		[PreserveSig()]
+		UInt32 GetCommandString(UInt32 idcmd, UInt32 uflags, IntPtr reserved, IntPtr commandstring, UInt32 cch);
+//		[MarshalAs(UnmanagedType.LPStr)]
+	}
+}
---
+++ trunk/PathToClipboard/PathToClipboard.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Runtime.InteropServices;
+
+namespace PathToClipboard	// WHY: PathToClipboardExtension doesn't work
+{
+	// Generate a new GUID and use it below in the class.
+	[ComVisible(true), System.Runtime.InteropServices.Guid("2E820618-9430-421b-8FA5-12BD9E31EEF3")]
+	public class PathToClipboard : ShellExtension.ContextMenu
+	{
+		override protected void AddMenuCommands(Win32Functions.Wrappers.Menu menuContext, System.Collections.Specialized.StringCollection filepaths)
+		{
+			if (filepaths.Count == 0)
+				return;
+
+			Win32Functions.Wrappers.MenuPopup menuPopup = new Win32Functions.Wrappers.MenuPopup("Path to clipboard");
+
+			// for each selected file, add all forms of its path to the menu, separated by--what else--separators
+			string strPaths = string.Empty;
+			string strNames = string.Empty;
+			foreach (string strPath in filepaths)
+			{
+				if (strPath != filepaths[0])
+					menuPopup.AppendMenuSeparator();
+
+				// C:\bin\test.txt
+				menuPopup.AppendMenuCommand(strPath + "\tPath", MyCommandHandler, strPath);
+				strPaths += strPath + System.Environment.NewLine;
+
+				// C:\bin\
+				string strName = strPath.Remove(strPath.LastIndexOf("\\"));
+				menuPopup.AppendMenuCommand(strName + "\tDirectory", MyCommandHandler, strName);
+
+				// test.txt
+				strName = strPath.Remove(0, strPath.LastIndexOf("\\") + 1);
+				menuPopup.AppendMenuCommand(strName + "\tFile", MyCommandHandler, strName);
+				strNames += strName + System.Environment.NewLine;
+
+//UNC paths
+//            // \\computer\c\bin\test.txt
+////string strUNC = "UNC format";
+//string strUNC = Win32Functions.Wrappers.WNet.WNetGetUniversalName(s);
+//            _idsFilepaths.Add(menuPopup.AppendMenuCommand(strUNC, MyCommandHandler), strUNC);
+			}
+
+			/*
+			 * If there more than one file is selected, add an "ALL PATHS" option.
+			 */
+			if (filepaths.Count > 1)
+			{
+				menuPopup.AppendMenuSeparator();
+
+				menuPopup.AppendMenuCommand("All paths\tPath", MyCommandHandler, strPaths);
+				// There's no point in doing "All directories" because they're all the same.
+				menuPopup.AppendMenuCommand("All names\tFile", MyCommandHandler, strNames);
+			}
+
+			menuContext.InsertMenuPopup(menuPopup);
+		}
+
+		override protected string GetCommandStringHelp(uint ixCmd)
+		{
+			return "Copy this text to the clipboard";
+		}
+
+
+		#region Menu Handlers
+
+		/// <summary>
+		/// Called by menu when the user selects a command.
+		/// </summary>
+		/// <typeparam name="T">Type of data stored with menu command</typeparam>
+		/// <param name="ix">Offset of menu command</param>
+		/// <param name="s">Data saved with command when it was added to the menu</param>
+		public void MyCommandHandler(uint ixOffset, object s)
+		{
+			System.Windows.Forms.Clipboard.SetText(s.ToString());
+		}
+
+		#endregion
+
+		#region Registration
+		static private string[] _filetypes = new string[] { "*", "Folder" };
+
+		[System.Runtime.InteropServices.ComRegisterFunctionAttribute()]
+		static public void RegisterServer(Type t)
+		{
+			// could also be "Directory" instead of "Folder"
+			ShellExtension.ContextMenu.RegisterServerHelper(t, _filetypes);
+		}
+
+		[System.Runtime.InteropServices.ComUnregisterFunctionAttribute()]
+		static public void UnregisterServer(Type t)
+		{
+			ShellExtension.ContextMenu.UnregisterServerHelper(t, _filetypes);
+		}
+
+		#endregion
+	}
+}
