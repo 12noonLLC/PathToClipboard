069087940c010fa7af5c497be2b6a295091c288f
Stefan Tucker <stefan@skst.com>
2009-12-12 03:30:08 +0000

    Build for .NET 3.5
    git-svn-id: svn://localhost/trunk@2176 11f071c2-d427-764a-a8f1-537ef3230c9f

--- trunk/PathToClipboard/Properties/AssemblyInfo.cs
+++ trunk/PathToClipboard/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("12noon")]
 [assembly: AssemblyProduct("PathToClipboard")]
-[assembly: AssemblyCopyright("© 2007-2008 12noon")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("© 2007-2009 12noon")]
+[assembly: AssemblyTrademark("PathToClipboard, 12noon")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.59.0")]
-[assembly: AssemblyFileVersion("1.0.59.0")]
+[assembly: AssemblyVersion("1.1.61.0")]
+[assembly: AssemblyFileVersion("1.1.61.0")]
